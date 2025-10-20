cbae14cf4a006ccbda0840066f521a4e4abed285
desyneer-lgtm <desyneer@gmail.com>
2025-10-20 15:14:00 +0100

    Update qc_socket.js

--- web/qc_socket.js
+++ web/qc_socket.js
@@ -1,8 +1,9 @@
 const QC_Socket = {
-  init: function(serverUrl) {
+  init: function("https://quadrachess-battle-of-four-kings.onrender.com") {
     this.socket = io(serverUrl);
     this.socket.on('connect', () => console.log('socket connected', this.socket.id));
   },
   sendMove: function(data) { this.socket.emit('move', data); },
   onMove: function(handler) { this.socket.on('move', handler); }
 };
+
